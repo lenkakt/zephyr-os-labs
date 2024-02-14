c3a6ccb2701f85c6c6c19664ba03a450e7da9e22
lenka.koskova.triskova <lenka.koskova.triskova@tul.cz>
2024-02-14 19:41:25 +0100

    Kernel lab added, tested, working

---
+++ kernel_lab/solutions/task_3.1.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+#define KL_TIMER_SEC 1
+
+int kl_calls = 0;
+
+void kl_work_handler(struct k_work *work)
+{    
+	kl_calls++;
+	printf("Hallo from kernel timer! I am in the %d th iteration.\n", kl_calls);
+}
+
+
+K_WORK_DEFINE(kl_work, kl_work_handler);
+
+void kl_timer_handler(struct k_timer *dummy)
+{
+    k_work_submit(&kl_work);
+}
+
+K_TIMER_DEFINE(kl_timer, kl_timer_handler, NULL);
+
+
+
+int main(void)
+{
+	printf("The timer application is starting. If you want to stop it, use Ctrl+A+X.\n");
+	
+	k_timer_start(&kl_timer, K_SECONDS(KL_TIMER_SEC), K_SECONDS(KL_TIMER_SEC));
+	return 0;
+}
---
+++ kernel_lab/src/main.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2012-2014 Wind River Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <zephyr/kernel.h>
+
+
+int main(void)
+{
+	printf("The timer application is not working - you must add your code. Stop this emulation by Ctrl+A+X.\n");
+	/* This file is waiting for your timer */
+
+	return 0;
+}
